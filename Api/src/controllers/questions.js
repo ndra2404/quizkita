6902c06ebf350f97a8fc3473a4ecc899454725ec
Indra PC <ndra2404@gmail.com>
2022-07-23 21:59:52 +0700

    aaaa

--- Api/src/controllers/questions.js
+++ Api/src/controllers/questions.js
@@ -6,16 +6,54 @@ pool.on("error", (err) => {
   console.error(err);
 });
 
+function getData(id, callback) {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      connection.query(
+        `SELECT * FROM question_answer WHERE question_id = ${id}`,
+        (err, rows) => {
+          connection.release();
+          if (err) {
+            callback(err, null);
+          } else {
+            callback(null, rows);
+          }
+        }
+      );
+    }
+  });
+}
 module.exports = {
   getAllQuestions(req, res) {
     pool.getConnection(function (err, connection) {
       if (err) throw err;
       connection.query("SELECT * FROM question", function (error, results) {
         if (error) throw error;
+
+        let dataresponse = [];
+        results.forEach((element) => {
+          dataresponse.push({
+            id: element.question_id,
+            question: element.question,
+            is_image: element.is_images,
+            question_image: element.question_image,
+            question_data: getData(element.question_id, (err, data) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    return data;
+                }
+                }
+            ),
+          });
+        });
+
         res.send({
           success: true,
           message: "Berhasil ambil data!",
-          data: results,
+          data: dataresponse,
         });
       });
       connection.release();
